1c8b2d21be6acce3d32e191c3d4f09a898950b2f
sathish <91046185+Sathishray21@users.noreply.github.com>
2021-10-16 11:50:42 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,20 @@
+import justpy as jp
+from webapp import layout
+from webapp import page
+
+class About(page.Page):
+    path = "/about"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+
+        lay = layout.DefaultLayout(a=wp)
+
+        container = jp.QPageContainer(a=lay)
+
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen")
+        jp.Div(a=div, text="This is the About page!", classes="text-4xl m-2")
+        jp.Div(a=div, text="""This is the instant english dictionary.""")
+        
+        return wp
+
